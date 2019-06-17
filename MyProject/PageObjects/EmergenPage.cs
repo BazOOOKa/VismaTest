11e3ed75481809848c410ad80cf3f3ff1576c54a
ildar.gataullin <ildar.gataullin@accenture.com>
2019-06-17 09:46:16 +0300

    Emergen task

---
+++ MyProject/PageObjects/EmergenPage.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject.PageObjects
+{
+    class EmergenPage
+    {
+        public EmergenPage(IWebDriver driver)
+        {
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.CssSelector, Using = ".masthead__logo")]
+        public IWebElement EurOfficePageTItle { get; set; }
+
+    }
+}
